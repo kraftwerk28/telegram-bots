dc39496be0c61421868c5739563536948500aec4
kraftwerk28 <kefirchik3@gmail.com>
2018-09-19 14:02:23 +0300

    msg checker improved

--- antispam28_bot/lib/message-checker.js
+++ antispam28_bot/lib/message-checker.js
@@ -1,14 +1,21 @@
 'use strict';
 
 const badChecker = /(vip|вип|btc|bitcoin|биткоин|porn|pron|порно|cp|канал|gastro)/i;
+const badUrls = [
+  /t\.me\//,
+  /instagram\.com\//,
+  /facebook\.com\//
+]
 
 module.exports = function bad(msg) {
   return (
     msg['entities'] &&
     (
       msg.entities.some((e) => e.type === 'text_link') ||
-      msg.entities.some((e) => e.type === 'url' &&
-        /t\.me\//.test(msg.text))
+      msg.entities.some((e) =>
+        e.type === 'url' &&
+        badUrls.some(u => u.test(msg.text))
+      )
     ) &&
     badChecker.test(msg.text)
   );
