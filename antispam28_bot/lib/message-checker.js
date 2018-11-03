8796055d6e4199c2a7d2b70468be34d8aafce5c3
kraftwerk28 <kefirchik3@gmail.com>
2018-11-03 14:38:57 +0200

    some improvements

--- antispam28_bot/lib/message-checker.js
+++ antispam28_bot/lib/message-checker.js
@@ -4,14 +4,18 @@ const badChecker = /(vip|вип|btc|bitcoin|биткоин|porn|pron|порно|
 const badUrls = [
   /t\.me\//,
   /instagram\.com\//,
-  /facebook\.com\//
+  /facebook\.com\//,
+  /telegram.me/,
+  /joinchat/
 ]
 
 module.exports = function bad(msg) {
   return (
     msg['entities'] &&
     (
-      msg.entities.some((e) => e.type === 'text_link' || e.type === 'mention') ||
+      msg.entities.some((e) =>
+        e.type === 'text_link' ||
+        e.type === 'mention') ||
       msg.entities.some((e) =>
         e.type === 'url' &&
         badUrls.some(u => u.test(msg.text))
