bf7ad8e70ade8bb3f9766e62f16becf4bac7d7b5
kraftwerk28 <kefirchik3@gmail.com>
2018-11-12 13:05:14 +0200

    Sobko improved

--- sobko_bot/index.js
+++ sobko_bot/index.js
@@ -13,33 +13,37 @@ let mId = 0;
 const chats = [];
 
 dateEvents.on('date', () => {
-    const d = new Date();
-    if (d.getDate() % 2 === 0)
-        now = '13:00 - 19:00';
-    else
-        now = '9:00 - 15:00';
-    if (d.getDay() < 1)
-        now = 'сьогодні Собко І. І. не працює';
+  const d = new Date();
+  if (d.getDate() % 2 === 0)
+    now = '13:00 - 19:00';
+  else
+    now = '9:00 - 15:00';
+  if (d.getDay() < 1)
+    now = 'сьогодні Собко І. І. не працює';
 })
 
 bot.on('text', msg => {
-    if (sobko.test(msg.text)) {
-        work(msg);
-    }
+  if (sobko.test(msg.text)) {
+    work(msg);
+  }
+});
+
+bot.onText(/\/sobko/, (msg, match) => {
+  work(msg);
 });
 
 const work = msg => {
-    if (!chats.some(val => val.chat_id === msg.chat.id))
-        chats.push({ id: msg.chat.id, mId: null });
+  if (!chats.some(val => val.chat_id === msg.chat.id))
+    chats.push({ id: msg.chat.id, mId: null });
 
-    const index = chats.findIndex(val => val.id === msg.chat.id);
-    if (chats[index].mId !== null)
-        bot.deleteMessage(chats[index].id, chats[index].mId);
-    bot.sendMessage(
-        msg.chat.id,
-        `<b>Графік роботи Собко І. І.:</b>\n${always}\nГрафік роботи Собко І. І. на сьогодні:\n<code>${now}</code>\nТелефони реєстратури: <code>204-85-62</code>, <code>204-95-93</code>`,
-        { parse_mode: 'html', reply_to_message_id: msg.message_id }
-    ).then(msg => {
-        chats[index].mId = msg.message_id;
-    });
+  const index = chats.findIndex(val => val.id === msg.chat.id);
+  if (chats[index].mId !== null)
+    bot.deleteMessage(chats[index].id, chats[index].mId);
+  bot.sendMessage(
+    msg.chat.id,
+    `<b>Графік роботи Собко І. І.:</b>\n${always}\nГрафік роботи Собко І. І. на сьогодні:\n<code>${now}</code>\nТелефони реєстратури: <code>204-85-62</code>, <code>204-95-93</code>`,
+    { parse_mode: 'html', reply_to_message_id: msg.message_id }
+  ).then(msg => {
+    chats[index].mId = msg.message_id;
+  });
 };
